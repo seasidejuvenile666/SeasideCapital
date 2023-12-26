ac24fa2ee4883fe5ca50f5f6268504df1789416c
seasidejuvenile666 <seasidejuvenile@gmail.com>
2023-12-27 01:02:51 +0800

    12-26 update10

--- src/pages/tool/api.js
+++ src/pages/tool/api.js
@@ -1,22 +1,22 @@
-async function checkWallets(addresses) {
-    const results = [];
+// async function checkWallets(addresses) {
+//     const results = [];
   
-    for (const address of addresses) {
-      const apiUrl = `https://starkrocket.xyz/api/check_wallet?address=${address}`;
+//     for (const address of addresses) {
+//       const apiUrl = `https://starkrocket.xyz/api/check_wallet?address=${address}`;
   
-      try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        results.push({ address, data });
-      } catch (error) {
-        results.push({ address, error: error.message });
-      }
-    }
+//       try {
+//         const response = await fetch(apiUrl);
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         const data = await response.json();
+//         results.push({ address, data });
+//       } catch (error) {
+//         results.push({ address, error: error.message });
+//       }
+//     }
   
-    return results;
-  }
+//     return results;
+//   }
   
-  module.exports = checkWallets;
+//   module.exports = checkWallets;
