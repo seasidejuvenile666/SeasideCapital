b66888130993cd6a09c2766d75e869f8219e51d7
seasidejuvenile666 <seasidejuvenile@gmail.com>
2023-12-27 01:23:46 +0800

    12-27 update14

---
+++ src/pages/airdrop-query.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// 最简单的函数式组件
+const SimpleFunctionalComponent = () => {
+  return (
+    <div>
+      <h1>Hello, I'm a simple functional component!</h1>
+    </div>
+  );
+};
+
+export default SimpleFunctionalComponent
