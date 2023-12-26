e65da9df64cb8d3d6e99e7e4327ab54e0d7c7382
seasidejuvenile666 <seasidejuvenile@gmail.com>
2023-12-27 00:30:00 +0800

    12-26 update7

--- src/pages/batch-query.js
+++ src/pages/batch-query.js
@@ -35,23 +35,32 @@ export default function BatchQuery() {
 
   const handleSubmit = async () => {
     setLoading(true); 
-  
+
     const addressesArray = addresses.split('\n').filter(address => address.trim() !== '');
     if (!addressesArray.length) {
       setLoading(false);
       return; // Add a check to avoid empty addressesArray
     }
-  
-    try {
-      const queryResults = await checkWallets(addressesArray);
-      // ...rest of your code remains the same
-    } catch (error) {
-      // Handle any potential errors here
-      console.error("Error fetching wallet data:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+
+  try {
+    const queryResults = await checkWallets(addressesArray);
+    setResults(queryResults);
+
+    const newData = queryResults.map((result, index) => ({
+      key: index.toString(),
+      address: result.address,
+      number: result.error ? `Error: ${result.error}` : result.data.result.points || 0,
+    }));
+
+    setData(newData); 
+    setLoading(false); 
+  } catch (error) {
+    // Handle any potential errors here
+    console.error("Error fetching wallet data:", error);
+  } finally {
+    setLoading(false);
+  }
+};
 
   const calculateTotalPoints = () => {
     let totalPoints = 0;
