6e37891fe5b46b1fe97f545676000c18ca0a1e88
0xSeasideJuvenile <105691608+seasidejuvenile666@users.noreply.github.com>
2024-01-22 21:37:45 +0800

    Update fly-starter.js

--- src/pages/airdrop-query/fly-starter.js
+++ src/pages/airdrop-query/fly-starter.js
@@ -51,7 +51,7 @@ export default function BatchQuery() {
         queryResults.push(result);
   
         // Delay between queries
-        await new Promise(resolve => setTimeout(resolve, 100));
+        await new Promise(resolve => setTimeout(resolve, 300));
       }
   
       setResults(queryResults);
